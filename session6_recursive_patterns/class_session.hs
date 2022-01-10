cabb3813c607bb29ebdc323d10e2fd446a30f086
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-01-10 19:46:45 +0700

    implement mapping

--- session6_recursive_patterns/class_session.hs
+++ session6_recursive_patterns/class_session.hs
@@ -3,17 +3,33 @@ data IntList
     | Cons Int IntList
     deriving (Show)
 
-addOneToAll :: IntList -> IntList
-addOneToAll Empty = Empty
-addOneToAll (Cons x xs) = Cons (x + 1) (addOneToAll xs)
-
 myIntList = Cons 2 (Cons (-3) (Cons 5 Empty))
 
-absAll :: IntList -> IntList
-absAll Empty = Empty
-absAll (Cons x xs) = Cons (abs x) (absAll xs)
+mapIntList :: (Int -> Int) -> IntList -> IntList
+mapIntList _ Empty = Empty
+mapIntList f (Cons x xs) = Cons (f x) (mapIntList f xs)
+
+addOne x = x + 1
+square x = x * x
+
+addOneToAll = mapIntList addOne
+absAll = mapIntList abs
+squareAll = mapIntList square
+
+-- addOneToAll :: IntList -> IntList
+-- addOneToAll Empty = Empty
+-- addOneToAll (Cons x xs) = Cons (x + 1) (addOneToAll xs)
+
+-- absAll :: IntList -> IntList
+-- absAll Empty = Empty
+-- absAll (Cons x xs) = Cons (abs x) (absAll xs)
+
+-- squareAll :: IntList -> IntList
+-- squareAll Empty = Empty
+-- squareAll (Cons x xs) = Cons (x * x) (squareAll xs)
 
 main :: IO ()
 main = do
     print (addOneToAll myIntList)
     print (absAll myIntList)
+    print (squareAll myIntList)
