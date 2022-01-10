9418bb4479c3042a22b2c4a07f07e5665deb911c
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-01-10 19:36:30 +0700

    add main

--- session6_recursive_patterns/class_session.hs
+++ session6_recursive_patterns/class_session.hs
@@ -8,3 +8,12 @@ addOneToAll Empty = Empty
 addOneToAll (Cons x xs) = Cons (x + 1) (addOneToAll xs)
 
 myIntList = Cons 2 (Cons (-3) (Cons 5 Empty))
+
+absAll :: IntList -> IntList
+absAll Empty = Empty
+absAll (Cons x xs) = Cons (abs x) (absAll xs)
+
+main :: IO ()
+main = do
+    print (addOneToAll myIntList)
+    print (absAll myIntList)
