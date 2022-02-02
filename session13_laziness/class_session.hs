4cee5e427dd1489541ffe4f59767dba5b236226c
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-02-02 20:58:14 +0700

    session13

--- session13_laziness/class_session.hs
+++ session13_laziness/class_session.hs
@@ -4,3 +4,15 @@ f1 m = [m, m]
 f2 :: Maybe a -> [a]
 f2 Nothing = []
 f2 (Just x) = [x]
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x : _) = Just x
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+take' :: Int -> [a] -> [a]
+take' n _ | n <= 0 = []
+take' _ [] = []
+take' n (x : xs) = x : take' (n - 1) xs
